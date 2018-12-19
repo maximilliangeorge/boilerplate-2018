e4d0ffae1939627152eaba23eb8d6eb1839dd736
Maximillian George <maximillian.george@gmail.com>
2018-12-19 12:07:31 +0100

    Add pixel effect and other things

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,63 @@
-$(document).ready(() => {
+document.addEventListener('DOMContentLoaded', () => {
 
-	console.log('Hello script!');
+	new Typewriter('#representations', {
+		strings: [''],
+		cursor: null,
+		autoStart: true,
+		loop: false,
+		delay: 25
+	}).typeString('EPRESENTATIONS and')
 
-});
+	setTimeout(() => {
+
+		new Typewriter('#reconfigurations', {
+			strings: [''],
+			cursor: null,
+			autoStart: true,
+			loop: false,
+			delay: 25
+		}).typeString('ECONFIGURATIONS of the')
+
+	}, 200)
+
+	setTimeout(() => {
+
+		new Typewriter('#digital', {
+			strings: [''],
+			cursor: null,
+			autoStart: true,
+			loop: false,
+			delay: 25
+		}).typeString('IGITAL in Swedish Literature and Art, 1950–2010')
+
+	}, 400)
+
+	const archiveTyper = new Typewriter('#archive', {
+		strings: [''],
+		cursor: null,
+		autoStart: true,
+		loop: false,
+		delay: 25
+	}).typeString('RCHIVE')
+
+	new Typewriter('#book', {
+		strings: [''],
+		cursor: null,
+		autoStart: true,
+		loop: false,
+		delay: 25
+	}).typeString('OOK')
+
+	new Typewriter('#conference', {
+		strings: [''],
+		cursor: null,
+		autoStart: true,
+		loop: false,
+		delay: 25
+	}).typeString('ONFERENCE')
+
+
+	document.getElementById('archive-link').addEventListener('mouseleave', () => {
+	})
+
+})
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,7 +1,2 @@
-import jQuery from 'jquery'
-window.$ = window.jQuery = jQuery
-
-import 'slick-carousel';
-
-import Vue from 'vue/dist/vue.common.js'
-window.Vue = Vue
+import Typewriter from 'typewriter-effect/dist/core'
+window.Typewriter = Typewriter
